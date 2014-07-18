4c13aab56cc4aadb74c6a202b84960f57942903d
Richard Kirkpatrick <rskirkpatrick22@gmail.com>
2014-07-17 19:00:33 -0500

    First commit
    Library for drawing buttons to TFT touch screen

---
+++ TouchScreenButtons.cpp
@@ -0,0 +1,106 @@
+/*
+   TouchScreenButton.cpp - Library for creating buttons for the Seeed Studio TFT touch screen.
+   Created by Richard Kirkpatrick, 19 June 2014
+*/
+
+#include "Arduino.h"
+#include "TouchScreenButtons.h"
+
+#include <stdint.h>
+#include <TouchScreen.h>
+#include <TouchScreenGeometry.h>
+#include <TouchSceenStrings.h>
+#include <TFT.h>
+#include <math.h>
+
+/*
+----------------------------------------------------------------
+                        Button Class
+----------------------------------------------------------------
+*/
+
+Button::Button()
+{
+    button = Rectangle();
+}
+
+Button::Button(const int myXStart, const int myYStart, const int myWidth, const int myHeight, unsigned int myBorderColor = 0xffff, unsigned int myFillColor = 0x0000)
+{
+    button = Rectangle(myXStart, myYStart, myWidth, myHeight, myBorderColor, myFillColor);
+}
+
+void Button::setValues(const int myXStart, const int myYStart, const int myWidth, const int myHeight)
+{
+    button.setUpperLeft(myXStart, myYStart);
+    button.setSize(myWidth, myHeight);
+}
+
+void Button::setBorderColor(unsigned int myBorderColor)
+{
+    button.setBorderColor(myBorderColor);
+}
+
+void Button::setFillColor(unsigned int myFillColor)
+{
+    button.setFillColor(myFillColor);
+}
+
+const int Button::getXStart()
+{
+    return button.getXStart();
+}
+
+const int Button::getYStart()
+{
+    return button.getYStart();
+}
+
+const int Button::getXEnd()
+{
+    return button.getXEnd();
+}
+
+const int Button::getYEnd()
+{
+    return button.getYEnd();
+}
+
+const int Button::getWidth()
+{
+    return button.getWidth();
+}
+
+const int Button::getHeight()
+{
+    return button.getHeight();
+}
+
+bool Button::isPressed(const int xInput, const int yInput)
+{
+    return ((xInput > button.getXStart() && xInput < button.getXEnd()) && (yInput > button.getYStart() && yInput < button.getYStart()));
+}
+
+void Button::draw()
+{
+    button.draw();
+}
+
+void Button::fill()
+{
+    button.fill();
+}
+
+void Button::buttonDisplay(unsigned int myBorderColor, unsigned int myFillColor)
+{
+    unsigned int originalBorderColor = button.getBorderColor(); ///< Saves the original border color
+    unsigned int originalFillColor = button.getFillColor(); ///< Saves the original fill color
+    button.setBorderColor(myBorderColor);
+    button.setFillColor(myFillColor);
+    button.draw();
+    delay(100);
+    button.setBorderColor(originalBorderColor);
+    button.setFillColor(originalFillColor);
+    button.draw();
+}
+
+
---
+++ TouchScreenButtons.h
@@ -0,0 +1,98 @@
+/*
+   TouchScreenButton.cpp - Library for creating buttons for the Seeed Studio TFT touch screen.
+   Created by Richard Kirkpatrick, 19 June 2014
+*/
+
+#ifndef TOUCHSCREENBUTTONS_H_INCLUDED
+#define TOUCHSCREENBUTTONS_H_INCLUDED
+
+#include "Arduino.h"
+#include <TouchScreenGeometry.h>
+#include <TouchSceenStrings.h>
+
+class Button {
+public:
+    ///@brief Default constructor for the Button class
+    Button();
+
+    /**
+    @brief Parameter constructor for the button class.
+    @param myXStart The starting x-coordinate of the button.
+    @param myYStart The starting y-coordinate of the button.
+    @param myWidth The width of the button.
+    @param myHeight The height of the button.
+    @param myBorderColor The border color of the button. Default color is white.
+    @param myFillColor The fill color of the button. Default color is black.
+    */
+    Button(const int myXStart, const int myYStart, const int myWidth, const int myHeight, unsigned int myBorderColor, unsigned int myFillColor);
+
+    /**
+    @brief Sets the values of the button instance.
+    @param myXStart The starting x-coordinate of the button.
+    @param myYStart The starting y-coordinate of the button.
+    @param myWidth The width of the button.
+    @param myHeight The height of the button.
+    @param myBorderColor The border color of the button. Default color is white.
+    @param myFillColor The fill color of the button. Default color is black.
+    */
+    void setValues(const int myXStart, const int myYStart, const int myWidth, const int myHeight);
+
+    /**
+    @brief Sets the border color of the button instance.
+    @param myBorderColor The border color of the button instance.
+    */
+    void setBorderColor(unsigned int myBorderColor);
+
+    /**
+    @brief Sets the fill color of the button instance.
+    @param myFillColor The fill color of the button instance.
+    */
+    void setFillColor(unsigned int myFillColor);
+
+    ///@brief Gets the left bound x-coordinate
+    const int getXStart();
+
+    ///@brief Gets the upper bound y-coordinate
+    const int getYStart();
+
+    ///@brief Gets the right bound x-coordinate
+    const int getXEnd();
+
+    ///@brief Gets the lower bound y-coordinate
+    const int getYEnd();
+
+    ///@brief Gets the width of the button
+    const int getWidth();
+
+    ///@brief Gets the height of the button
+    const int getHeight();
+
+    /**
+    @brief Determines if the button was pressed
+    @param xScreen The x-coordinate of the user's input.
+    @param yScreen The y-coordinate of the user's input.
+    @return Returns True if the user pressed the button within the boundaries.  False otherwise.
+    */
+    bool isPressed(const int xScreen, const int yScreen);
+
+    ///@brief Draws the button
+    void draw();
+
+    ///@brief Fills the button
+    void fill();
+
+    /**
+    @brief Displays the button when pressed.
+    @details The default border color is RED and the default fill color is BLACK.  Button's border color and fill color is updated.
+             The button is then drawn.  After a 100 ms delay, the button is reset back to its original values.
+    @param myBorderColor The border color of the button. Default color is red.
+    @param myFillColor The fill color of the button. Default color is black.
+    */
+    void buttonDisplay(unsigned int myBorderColor = 0xf800, unsigned int myFillColor = 0x0000);
+
+private:
+    Rectangle button;
+};
+
+
+#endif // TOUCHSCREENBUTTON_H_INCLUDED
