7025397ecfe98f4c97ed26a2645b8fe67dad15c4
Richard Kirkpatrick <rskirkpatrick22@gmail.com>
2014-08-06 23:08:11 -0500

    Resolving conflict

--- TouchScreenButtons.h
+++ TouchScreenButtons.h
@@ -35,6 +35,7 @@ public:
     @param myBorderColor The border color of the button. Default color is white.
     @param myFillColor The fill color of the button. Default color is black.
     */
+<<<<<<< HEAD
     Button(const int myXStart,
            const int myYStart,
            const int myWidth,
@@ -61,6 +62,9 @@ public:
            unsigned int myBorderColor = 0xffff,
            unsigned int myFillColor = 0x0000,
            unsigned int myTextColor = 0xffff);
+=======
+    Button(const int myXStart, const int myYStart, const int myWidth, const int myHeight, unsigned int myBorderColor, unsigned int myFillColor);
+>>>>>>> parent of 37dcba3... Updated default border color and fill color
 
     /**
     @brief Sets the values of the button instance.
